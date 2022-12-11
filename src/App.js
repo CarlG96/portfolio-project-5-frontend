dfae45f1b03996327667447a1ec3820f55a2e180
CarlG96 <carl.gardiner1@hotmail.com>
2022-12-11 15:37:03 +0000

    added font types to taskosaurus and checked they are importing correctly

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
+      <h1>Taskosaurus</h1>
+      <h2>Taskosaurus</h2>
+      <p>Lorem Ipsum adbhbhdbhbchjabcbcabac</p>
     </div>
   );
 }
