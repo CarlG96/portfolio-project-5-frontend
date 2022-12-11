9c6e7835a0db0f894be1249b7aed63bf42c7d612
CarlG96 <carl.gardiner1@hotmail.com>
2022-12-11 20:26:53 +0000

    style: Add font awesome icons to the navigation links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import logo from '../assets/taskosaurus-logo.png';
+import styles from '../styles/NavBar.module.css';
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
-      <Container>
+    <Navbar className={`${styles.NavBar}`} expand="md" fixed="top">
+      <Container fluid>
         <Navbar.Brand href="#home"a><img src={logo} alt="logo" height="100" width="100"/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>Current Tasks</Nav.Link>
-            <Nav.Link>Archived Tasks</Nav.Link>
-            <Nav.Link>Create Task</Nav.Link>
-            <Nav.Link>Sign In</Nav.Link>
-            <Nav.Link>Sign Up</Nav.Link>
-            <Nav.Link>Logout</Nav.Link>
-            <Nav.Link>Profile</Nav.Link>
+            <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
+            <Nav.Link><i class="fa-sharp fa-solid fa-list-check" />Current Tasks</Nav.Link>
+            <Nav.Link><i class="fa-sharp fa-solid fa-vault" />Archived Tasks</Nav.Link>
+            <Nav.Link><i className="fas fa-solid fa-pen" />Create Task</Nav.Link>
+            <Nav.Link><i className="fas fa-sign-in" />Sign In</Nav.Link>
+            <Nav.Link><i className="fas fa-user-plus" />Sign Up</Nav.Link>
+            <Nav.Link><i class="fa-sharp fa-solid fa-arrow-right-from-bracket" />Logout</Nav.Link>
+            <Nav.Link><i class="fa-solid fa-address-card" />Profile</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
