1a0e50eefcae58a745533ca330a5e7f938a1562b
CarlG96 <carl.gardiner1@hotmail.com>
2022-12-22 15:40:06 +0000

    added boilerplate form for signup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import styles from "./App.css";
 import NavBar from "./components/NavBar";
+import SignUpForm from "./pages/auth/SignUpForm";
 import WelcomeText from "./components/WelcomeText";
 import {Route, Switch} from 'react-router-dom';
 import "./api/axiosDefaults";
@@ -13,7 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={()=> <h1>Home Page</h1>} />
           <Route exact path="/signin" render={()=> <h1>Sign In</h1>} />
-          <Route exact path="/signup" render={()=> <h1>Sign In</h1>} />
+          <Route exact path="/signup" render={()=> <SignUpForm />} />
           <Route exact path="/logout" render={()=> <h1>Sign In</h1>} />
           <Route exact path="/currenttasks" render={()=> <h1>Sign In</h1>} />   
           <Route exact path="/archivedtasks" render={()=> <h1>Sign In</h1>} />
