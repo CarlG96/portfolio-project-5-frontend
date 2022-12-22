9379d31b823133355299ececb9724d4520fbc2d5
CarlG96 <carl.gardiner1@hotmail.com>
2022-12-22 13:47:33 +0000

    feat: Add routes to navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import { Container } from "react-bootstrap";
 import styles from "./App.css";
 import NavBar from "./components/NavBar";
 import WelcomeText from "./components/WelcomeText";
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Container fluid> 
-        <WelcomeText />
+      <Container>
+        <Switch>
+          <Route exact path="/" render={()=> <h1>Home Page</h1>} />
+          <Route exact path="/signin" render={()=> <h1>Sign In</h1>} />
+          <Route exact path="/signup" render={()=> <h1>Sign In</h1>} />
+          <Route exact path="/logout" render={()=> <h1>Sign In</h1>} />
+          <Route exact path="/currenttasks" render={()=> <h1>Sign In</h1>} />   
+          <Route exact path="/archivedtasks" render={()=> <h1>Sign In</h1>} />
+          <Route exact path="/createtask" render={()=> <h1>Sign In</h1>} />
+          <Route exact path="/profile" render={()=> <h1>Sign In</h1>} />
+          <Route render={()=><p>Page not found!</p>} />
+        </Switch> 
+        {/* <WelcomeText /> */}
       </Container>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,52 @@
 import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
-import logo from '../assets/taskosaurus-logo.png';
-import styles from '../styles/NavBar.module.css';
+import logo from "../assets/taskosaurus-logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={`${styles.NavBar}`} expand="md">
       <Container fluid>
-        <Navbar.Brand href="#home"a><img src={logo} alt="logo" height="100" width="100"/></Navbar.Brand>
+        <NavLink to="/" className={styles.NavLink}>
+          <Navbar.Brand href="#home" a>
+            <img src={logo} alt="logo" height="100" width="100" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
-            <Nav.Link><i className="fa-sharp fa-solid fa-list-check" />Current Tasks</Nav.Link>
-            <Nav.Link><i className="fa-sharp fa-solid fa-vault" />Archived Tasks</Nav.Link>
-            <Nav.Link><i className="fas fa-solid fa-pen" />Create Task</Nav.Link>
-            <Nav.Link><i className="fas fa-sign-in" />Sign In</Nav.Link>
-            <Nav.Link><i className="fas fa-user-plus" />Sign Up</Nav.Link>
-            <Nav.Link><i className="fa-sharp fa-solid fa-arrow-right-from-bracket" />Logout</Nav.Link>
-            <Nav.Link><i className="fa-solid fa-address-card" />Profile</Nav.Link>
+            <NavLink to="/" exact className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink to="/currenttasks" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fa-sharp fa-solid fa-list-check" />
+              Current Tasks
+            </NavLink>
+            <NavLink to="/archivedtasks" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fa-sharp fa-solid fa-vault" />
+              Archived Tasks
+            </NavLink>
+            <NavLink to="/createtask" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fas fa-solid fa-pen" />
+              Create Task
+            </NavLink>
+            <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fas fa-sign-in" />
+              Sign In
+            </NavLink>
+            <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fas fa-user-plus" />
+              Sign Up
+            </NavLink>
+            <NavLink to="/logout" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fa-sharp fa-solid fa-arrow-right-from-bracket" />
+              Logout
+            </NavLink>
+            <NavLink to="/profile" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fa-solid fa-address-card" />
+              Profile
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
