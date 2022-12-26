46ae2d58ca3d8ab3954e0e8038773fb699b8f0ad
CarlG96 <carl.gardiner1@hotmail.com>
2022-12-26 10:18:02 +0000

    feat: Start adding functionality to the signin form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Container } from "react-bootstrap";
-import styles from "./App.css";
+import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import SignUpForm from "./pages/auth/SignUpForm";
 import WelcomeText from "./components/WelcomeText";
 import {Route, Switch} from 'react-router-dom';
 import "./api/axiosDefaults";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
@@ -13,9 +14,9 @@ function App() {
       <Container>
         <Switch>
           <Route exact path="/" render={()=> <h1>Home Page</h1>} />
-          <Route exact path="/signin" render={()=> <h1>Sign In</h1>} />
+          <Route exact path="/signin" render={()=> <SignInForm />} />
           <Route exact path="/signup" render={()=> <SignUpForm />} />
-          <Route exact path="/logout" render={()=> <h1>Sign In</h1>} />
+          <Route exact path="/logout" render={()=> <h1>Logout</h1>} />
           <Route exact path="/currenttasks" render={()=> <h1>Sign In</h1>} />   
           <Route exact path="/archivedtasks" render={()=> <h1>Sign In</h1>} />
           <Route exact path="/createtask" render={()=> <h1>Sign In</h1>} />
