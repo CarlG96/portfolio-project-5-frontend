cc93443c292555fd8a07ccb9147bce238de9efe1
CarlG96 <carl.gardiner1@hotmail.com>
2022-12-26 11:07:27 +0000

    fix: Show different icons in navbar depending on loggedin/ loggedout status

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,41 @@ import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
   const currentUser = useContext(CurrentUserContext);
-  // const loggedOutIcons = 
-  // const loggedInIcons = 
+  const loggedOutIcons = <>
+<NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fas fa-sign-in" />
+              Sign In
+            </NavLink>
+            <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
+              <i className="fas fa-user-plus" />
+              Sign Up
+            </NavLink></>
+  const loggedInIcons = <><NavLink to="/currenttasks" className={styles.NavLink} activeClassName={styles.Active}>
+  <i className="fa-sharp fa-solid fa-list-check" />
+  Current Tasks
+</NavLink>
+<NavLink to="/archivedtasks" className={styles.NavLink} activeClassName={styles.Active}>
+  <i className="fa-sharp fa-solid fa-vault" />
+  Archived Tasks
+</NavLink>
+<NavLink to="/createtask" className={styles.NavLink} activeClassName={styles.Active}>
+  <i className="fas fa-solid fa-pen" />
+  Create Task
+</NavLink>
+<NavLink to="/profile" className={styles.NavLink} activeClassName={styles.Active}>
+  <i className="fa-solid fa-address-card" />
+  Profile
+</NavLink>
+<NavLink to="/logout" className={styles.NavLink} activeClassName={styles.Active}>
+  <i className="fa-sharp fa-solid fa-arrow-right-from-bracket" />
+  Logout
+</NavLink></>
 
   return (
     <Navbar className={`${styles.NavBar}`} expand="md">
       <Container fluid>
         <NavLink to="/" className={styles.NavLink}>
-          <Navbar.Brand href="#home" a>
+          <Navbar.Brand >
             <img src={logo} alt="logo" height="100" width="100" />
           </Navbar.Brand>
         </NavLink>
@@ -24,34 +51,7 @@ const NavBar = () => {
             <NavLink to="/" exact className={styles.NavLink} activeClassName={styles.Active}>
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink to="/currenttasks" className={styles.NavLink} activeClassName={styles.Active}>
-              <i className="fa-sharp fa-solid fa-list-check" />
-              Current Tasks
-            </NavLink>
-            <NavLink to="/archivedtasks" className={styles.NavLink} activeClassName={styles.Active}>
-              <i className="fa-sharp fa-solid fa-vault" />
-              Archived Tasks
-            </NavLink>
-            <NavLink to="/createtask" className={styles.NavLink} activeClassName={styles.Active}>
-              <i className="fas fa-solid fa-pen" />
-              Create Task
-            </NavLink>
-            <NavLink to="/signin" className={styles.NavLink} activeClassName={styles.Active}>
-              <i className="fas fa-sign-in" />
-              Sign In
-            </NavLink>
-            <NavLink to="/signup" className={styles.NavLink} activeClassName={styles.Active}>
-              <i className="fas fa-user-plus" />
-              Sign Up
-            </NavLink>
-            <NavLink to="/logout" className={styles.NavLink} activeClassName={styles.Active}>
-              <i className="fa-sharp fa-solid fa-arrow-right-from-bracket" />
-              Logout
-            </NavLink>
-            <NavLink to="/profile" className={styles.NavLink} activeClassName={styles.Active}>
-              <i className="fa-solid fa-address-card" />
-              Profile
-            </NavLink>
+            { currentUser ? loggedInIcons : loggedOutIcons }
           </Nav>
         </Navbar.Collapse>
       </Container>
