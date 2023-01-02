3ea5a6a3490cf9da0e0a108f36b5c8235468fd13
CarlG96 <carl.gardiner1@hotmail.com>
2023-01-02 13:11:29 +0000

    feat: Get taskform page sending off data to backend without 400 errors

--- src/pages/tasks/TaskCreateForm.js
+++ src/pages/tasks/TaskCreateForm.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Alert, Button, Form } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
 
 const TaskCreateForm = () => {
+  const [errors, setErrors] = useState({});
   const [taskData, setTaskData] = useState({
     title: "",
-    dueDate: "",
-    priority: "",
+    due_date: "",
+    priority: "Must do",
     description: "",
   });
 
-  const { title, dueDate, priority, description } = taskData;
+  const { title, due_date, priority, description } = taskData;
+  const history = useHistory();
 
   const handleChange = (event) => {
     setTaskData({
@@ -18,8 +22,28 @@ const TaskCreateForm = () => {
     });
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    formData.append("title", title);
+    formData.append("due_date", due_date);
+    formData.append("priority", priority);
+    formData.append("description", description);
+
+    try {
+      const { data } = await axiosReq.post("/tasks/", formData);
+      history.push(`/tasks/${data.id}`);
+    } catch (err) {
+      console.log(err);
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+      }
+    }
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group controlId="title">
         <Form.Label className="d-none">Title</Form.Label>
         <Form.Control
@@ -30,23 +54,42 @@ const TaskCreateForm = () => {
           onChange={handleChange}
         ></Form.Control>
       </Form.Group>
-      <Form.Group controlId="dueDate">
+      {errors?.title?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group controlId="due_date">
         <Form.Label className="d-none">Due date</Form.Label>
         <Form.Control
           type="datetime-local"
-          name="dueDate"
-          value={dueDate}
+          name="due_date"
+          value={due_date}
           onChange={handleChange}
         ></Form.Control>
       </Form.Group>
+      {errors?.due_date?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
       <Form.Group controlId="priority">
         <Form.Label className="d-none">Priority</Form.Label>
-        <Form.Control as="select" name="priority" value={priority} onChange={handleChange}>
-          <option>Must Do</option>
-          <option>Might Do</option>
-          <option>Can Do</option>
+        <Form.Control
+          as="select"
+          name="priority"
+          onChange={handleChange}
+        >
+          <option value="Must do">Must do</option>
+          <option value="Might do">Might do</option>
+          <option value="Can do">Can do</option>
         </Form.Control>
       </Form.Group>
+      {errors?.priority?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
       <Form.Group controlId="description">
         <Form.Label className="d-none">Description</Form.Label>
         <Form.Control
@@ -58,8 +101,14 @@ const TaskCreateForm = () => {
           onChange={handleChange}
         />
       </Form.Group>
+      {errors?.description?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Button type="submit">Create Task</Button>
     </Form>
   );
-}
+};
 
 export default TaskCreateForm;
