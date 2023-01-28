fe405e6c75f6376093c1c8436b4e9e1bd2b38f7e
CarlG96 <carl.gardiner1@hotmail.com>
2023-01-28 14:57:47 +0000

    feat: set conditional choices for editing and approach finishing edit capability

--- src/pages/tasks/DetailedTask.js
+++ src/pages/tasks/DetailedTask.js
@@ -4,9 +4,14 @@ import { Alert, Button, Container, Form } from "react-bootstrap";
 import { mockComponent } from "react-dom/test-utils";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import btnStyles from "../../styles/Button.module.css";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const DetailedTask = (props) => {
   const [errors, setErrors] = useState({});
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [changeDate, setChangeDate] = useState(false);
+  const [removeChangeDateButton, setRemoveChangeDateButton] = useState(false);
   const {
     date_created,
     date_updated,
@@ -22,6 +27,8 @@ const DetailedTask = (props) => {
     taskPage,
   } = props;
 
+  const history = useHistory();
+
   const [taskData, setTaskData] = useState({
     date_created: date_created,
     date_updated: date_updated,
@@ -33,7 +40,7 @@ const DetailedTask = (props) => {
     owner: owner,
     priority: priority,
     state: state,
-    title: title
+    title: title,
   });
 
   const handleChange = (event) => {
@@ -43,9 +50,36 @@ const DetailedTask = (props) => {
     });
   };
 
-  const handleSubmit = async (event) => {};
+  const handleEdit = () => {
+    setIsDisabled(false);
+  };
+
+  const handleChangeDate = () => {
+    setChangeDate(true);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("description", description);
+    formData.append("priority", priority);
+    formData.append("due_date", due_date);
+    formData.append("state", state);
+
+    try {
+      const { data } = await axiosReq.put(`/tasks/${id}`, formData);
+      history.push(`/tasks/${id}`);
+    } catch (err) {
+      console.log(err);
+      if (err.response?.status !== 401) {
+        setErrors(err.response?.data);
+      }
+    }
+  };
 
   const currentUser = useCurrentUser();
+
   return (
     <Container>
       <Form onSubmit={handleSubmit} className="text-center mt-3">
@@ -56,6 +90,7 @@ const DetailedTask = (props) => {
             name="title"
             placeholder="Title"
             defaultValue={title}
+            disabled={isDisabled}
             onChange={handleChange}
             className="text-center"
           ></Form.Control>
@@ -73,6 +108,7 @@ const DetailedTask = (props) => {
             name="description"
             defaultValue={description}
             placeholder="Description here"
+            disabled={isDisabled}
             onChange={handleChange}
             className="text-center"
           />
@@ -87,6 +123,7 @@ const DetailedTask = (props) => {
           <Form.Control
             as="select"
             name="priority"
+            disabled={isDisabled}
             onChange={handleChange}
             className="text-center"
             defaultValue={priority}
@@ -104,20 +141,22 @@ const DetailedTask = (props) => {
         <Form.Group controlId="due_date">
           <Form.Label>Due date</Form.Label>
 
-          <Form.Control
-            type="text"
-            className="text-center"
-            defaultValue={moment(due_date).format("DD/mm/yyyy hh:mm")}
-            disabled
-          ></Form.Control>
-          <p className="mt-3">Change Date?</p>
-          <Form.Control
+          {changeDate? (
+            <Form.Control
             type="datetime-local"
             name="due_date"
             defaultValue={due_date}
+            disabled={isDisabled}
             onChange={handleChange}
             className="text-center"
           ></Form.Control>
+          ): (<Form.Control
+            type="text"
+            className="text-center"
+            name="due_date"
+            defaultValue={moment(due_date).format("DD/mm/yyyy hh:mm")}
+            disabled
+          ></Form.Control>)} 
         </Form.Group>
         {errors?.due_date?.map((message, idx) => (
           <Alert variant="warning" key={idx}>
@@ -127,11 +166,12 @@ const DetailedTask = (props) => {
         <Form.Group controlId="state">
           <Form.Label>State</Form.Label>
           <Form.Control
-          as="select"
-          name="state"
-          defaultValue={state}
-          onChange={handleChange}
-          className="text-center"
+            as="select"
+            name="state"
+            defaultValue={state}
+            disabled={isDisabled}
+            onChange={handleChange}
+            className="text-center"
           >
             <option value="Current">Current</option>
             <option value="Archived">Archived</option>
@@ -142,13 +182,32 @@ const DetailedTask = (props) => {
             {message}
           </Alert>
         ))}
-        <Button
+        {isDisabled ? (
+          <></>
+        ) : (
+          <Button
             type="submit"
             className={`${btnStyles.Button} ${btnStyles.Bright}`}
           >
-            Edit Task?
+            Save
           </Button>
+        )}
       </Form>
+      {isDisabled ? (
+        <Button
+          onClick={handleEdit}
+          className={`${btnStyles.Button} ${btnStyles.Bright}`}
+        >
+          Edit Task?
+        </Button>
+      ) : (
+        <Button
+          onClick={handleChangeDate}
+          className={`${btnStyles.Button} ${btnStyles.Bright}`}
+        >
+          Change Date?
+        </Button>
+      )}
     </Container>
   );
   // // the .. needs to replace with edit and delete capability.
