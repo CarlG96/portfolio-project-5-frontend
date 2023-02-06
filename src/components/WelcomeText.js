a906eabe1d34623f64dac064e43497340b630f72
CarlG96 <carl.gardiner1@hotmail.com>
2023-02-06 18:54:41 +0000

    style: change welcome page styling to be more responsive

--- src/components/WelcomeText.js
+++ src/components/WelcomeText.js
@@ -21,7 +21,7 @@ const WelcomeText = () => {
         You can view a task from here by clicking on one. Once in the <em>Task View</em> you'll be able to update, delete or mark your tasks as complete.
         Completing a task sends it to the <em>Archived Tasks</em> section, so you never forget what you've done!
       </p>
-      <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}>
+      <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright} mb-3`}>
         <Link className={btnStyles.Link} to="/signin">Click here to sign up!</Link>
       </Button>
     </div>
