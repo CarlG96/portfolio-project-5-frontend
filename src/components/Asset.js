583ed9d6f44d16cd288c11fec27a6b02bc492b91
CarlG96 <carl.gardiner1@hotmail.com>
2023-02-09 11:26:00 +0000

    style: Style asset spinner and noresults, need taskdetail spinner and no results working next

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -3,7 +3,7 @@ import { Container, Spinner } from "react-bootstrap";
 
 const Asset = ({ spinner, src, message }) => {
   return (
-    <Container className={`p-4`}>
+    <Container className={`p-4 text-center`}>
       {spinner && <Spinner animation="border" />}
       {src && <img src={src} alt={message} />}
       {message && <p className="mt-4">{message}</p>}
--- src/components/NoResults.js
+++ src/components/NoResults.js
@@ -4,7 +4,7 @@ import styles from "../styles/NoResults.module.css";
 
 const NoResults = (props) => {
   return (
-    <Container className={`text center mt-3 ${styles.NoResults}`}>
+    <Container className={`text-center mt-3 ${styles.NoResults}`}>
         <h1>{props.message}</h1>
     </Container>
   )
