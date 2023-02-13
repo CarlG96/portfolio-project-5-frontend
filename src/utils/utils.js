6ecfd70e2bcbf3ebe1d5a7ceace6d8d2393f72aa
CarlG96 <carl.gardiner1@hotmail.com>
2023-02-13 19:33:06 +0000

    docs: Add docstrings to utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -23,15 +23,18 @@ export const fetchMoreData = async (resource, setResource) => {
   }
 };
 
+// Function to set token timestamp
 export const setTokenTimestamp = (data) => {
   const refreshTokenTimestamp = jwtDecode(data?.refresh_token).exp;
   localStorage.setItem("refreshTokenTimeStamp", refreshTokenTimestamp);
 };
 
+// Function to return bool of whether or not it should refresh token
 export const shouldRefreshToken = () => {
   return !!localStorage.getItem("refreshTokenTimestamp");
 };
 
+// Function to remove token timestamp
 export const removeTokenTimestamp = () => {
   localStorage.removeItem("refreshTokenTimestamp");
 }
