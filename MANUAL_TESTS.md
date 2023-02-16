10a2d3739b2ed1e4da8319ee8194079578f6dbf8
CarlG96 <carl.gardiner1@hotmail.com>
2023-02-16 19:05:28 +0000

    fix: Add spinner and loading logic to welcome page buttons to make it look better

--- MANUAL_TESTS.md
+++ MANUAL_TESTS.md
@@ -6,14 +6,25 @@ Here are a list of manual tests carried out to ensure that the Taskosaurus front
 
 ## Welcome Page
 
-* Test: A non-logged in user should see the 'Click here to sign up!' button.
-* Result: A non-logged in user can see the button.
+* The Welcome Page or the landing page introduces people to Taskosaurus.
 
-* Test: The 'Click here to sign up!' button should send the non-logged in user to the Sign Up Page.
-* Result: The user is directed to the Sign Up Page.
+    * Test: A non-logged in user should see the 'Click here to sign up!' button.
+    * Result: A non-logged in user can see the button.
 
-* Test: A logged-in user should not be able to see the 'Click here to sign up!' button.
-* Result: 
+    * Test: The 'Click here to sign up!' button should send the non-logged in user to the Sign Up Page.
+    * Result: The user is directed to the Sign Up Page.
+
+    * Test: A logged-in user should not be able to see the 'Click here to sign up!' button.
+    * Result: A logged-in user cannot see this button.
+
+    * Test: A logged-in user should be able to see the 'Click here to create a Task!' and 'Click here to create an Event!' buttons.
+    * Result: A logged-in user can see these buttons.
+
+    * Test: The 'Click here to create a Task!' button should direct the user to the Create Task Page.
+    * Result: The user is directed there.
+
+    * Test: The 'Click here to create an Event!' button should direct the user to the Create Event Page.
+    * Result: The user is directed there.
 
 ## Sign In Page
 
--- src/components/WelcomeText.js
+++ src/components/WelcomeText.js
@@ -1,42 +1,72 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import styles from '../styles/WelcomeText.module.css';
-import { Link } from 'react-router-dom';
-import btnStyles from '../styles/Button.module.css';
-import { useCurrentUser } from '../contexts/CurrentUserContext';
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import styles from "../styles/WelcomeText.module.css";
+import { Link } from "react-router-dom";
+import btnStyles from "../styles/Button.module.css";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+import Asset from "./Asset";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 /*
-* Component used in conjunction with the HeroImage component to create the
-* landing page. One-time-use component added to its own file for ease of 
-* readability elsewhere and to allow ease of applying CSS rules to the
-* component.
-*/
+ * Component used in conjunction with the HeroImage component to create the
+ * landing page. One-time-use component added to its own file for ease of
+ * readability elsewhere and to allow ease of applying CSS rules to the
+ * component.
+ */
 
 const WelcomeText = () => {
   const currentUser = useCurrentUser();
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+useEffect(() => {
+  const timeOut = setTimeout(() => {
+    setHasLoaded(true);
+  }, 500)
+  setHasLoaded(false);
+  return () => clearTimeout(timeOut);
+}, [currentUser]);
 
   return (
     <div className={styles.WelcomeText}>
       <h1 className={styles.Header}>Welcome to Taskosaurus!</h1>
       <p className={styles.Paragraph}>
         Taskosaurus is an app that allows you to keep track of Tasks and Events.
-        You can start by signing up and clicking on the <em>Create Task</em> or <em>Create Event</em> links. Your Tasks will be listed in the <em>Tasks</em> or <em>Events</em> sections.
-        You can view a Task or Event from there by clicking on one. From there, you can update or delete your Tasks and Events.
-        You can view archived Tasks or past Events by clicking the respective buttons in the <em>Tasks</em> and <em>Events</em> sections.
+        You can start by signing up and clicking on the <em>Create Task</em> or{" "}
+        <em>Create Event</em> links. Your Tasks will be listed in the{" "}
+        <em>Tasks</em> or <em>Events</em> sections. You can view a Task or Event
+        from there by clicking on one. From there, you can update or delete your
+        Tasks and Events. You can view archived Tasks or past Events by clicking
+        the respective buttons in the <em>Tasks</em> and <em>Events</em>{" "}
+        sections.
       </p>
-      {currentUser?.profile_id ? (<>
-        <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright} mb-3`}>
-        <Link className={btnStyles.Link} to="/createtask">Click here to create a Task!</Link>
-      </Button>
-      <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright} mb-3`}>
-        <Link className={btnStyles.Link} to="/createevent">Click here to create an Event!</Link>
-      </Button></>) : (
-      <Button className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright} mb-3`}>
-        <Link className={btnStyles.Link} to="/signup">Click here to sign up!</Link>
-      </Button>)
-      }
+      {hasLoaded ? (currentUser?.profile_id ? (
+        <>
+          <Button
+            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright} mb-3`}
+          >
+            <Link className={btnStyles.Link} to="/createtask">
+              Click here to create a Task!
+            </Link>
+          </Button>
+          <Button
+            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright} mb-3`}
+          >
+            <Link className={btnStyles.Link} to="/createevent">
+              Click here to create an Event!
+            </Link>
+          </Button>
+        </>
+      ) : (
+        <Button
+          className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright} mb-3`}
+        >
+          <Link className={btnStyles.Link} to="/signup">
+            Click here to sign up!
+          </Link>
+        </Button>
+      )) : (<Asset spinner />)}
     </div>
-  )
-}
+  );
+};
 
-export default WelcomeText
+export default WelcomeText;
